02f4c12195f0497f024efb9965908888dcf49e2b
nazmul-2020 <nazmulcpi2012@gmail.com>
2022-02-16 23:25:45 +0600

    strat

---
+++ js/app.js
@@ -0,0 +1,10 @@
+document.getElementById('cal-btn').addEventListener('click', function(){
+    const incomeInput = document.getElementById('income-input');
+    const incomeInputText = parseFloat(incomeInput);
+    console.log(incomeInputText)
+    // const incomeInputAmount = incomeInputText.value;
+
+    // console.log(22)
+    const foodInput = document.getElementById('food-input');
+    
+})

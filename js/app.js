97b8945c7ce47e7c035737e88a588605949b589e
nazmul-2020 <nazmulcpi2012@gmail.com>
2022-02-17 13:08:01 +0600

    css add

--- js/app.js
+++ js/app.js
@@ -1,27 +1,46 @@
+        //----------------------  Calculate ----------------------//
+
 document.getElementById('cal-btn').addEventListener('click', function(){
+    
+    // ------------ Income
     const incomeInput = document.getElementById('income-input');
     const incomeInputText = parseFloat(incomeInput.value);
-
+    
+    // ------------------ Food
     const foodInput = document.getElementById('food-input');
     const foodInputText = parseFloat(foodInput.value);
-
+    foodInput.value = '';
+    // ---------------- rent
     const rentInput = document.getElementById('rent-input');
     const rentInputText = parseFloat(rentInput.value);
-
+    rentInput.value = '';
+    // -------------- clothes
     const clothesInput = document.getElementById('clothes-input');
     const clothesInputText = parseFloat(clothesInput.value);
+    clothesInput.value = '';
 
-    const totalCost = foodInputText + rentInputText + clothesInputText;
-
+    // -------------Total-expenses
     const totalExpensesInput = document.getElementById('total-expenses');
     const totalExpensesAmount = parseFloat(totalExpensesInput.innerText);
+    const totalCost = foodInputText + rentInputText + clothesInputText;
      totalExpensesInput.innerText = totalExpensesAmount + totalCost;
-     
+    
+    // ------------------------Balance
      const restMoneyInput = document.getElementById('rest-money');
      const restMoneyInputAmount = parseFloat(restMoneyInput.innerText);
      restMoneyInput.innerText = incomeInputText - totalCost;
-
-    // console.log(totalCost)
-
+    
 
 })
+    //----------------- Save --------------------------//
+
+    document.getElementById('save-btn').addEventListener('click', function(){
+
+        const incomeInput = document.getElementById('income-input');
+        const incomeInputText = parseFloat(incomeInput.value); 
+        const saveInput = document.getElementById('save-btn');
+        const saveInputText = parseFloat(saveInput.value);
+        const savingAmount = document.getElementById('saving-amount');
+        const savingAmountT = parseFloat(savingAmount.innerText);
+        savingAmount.innerText = incomeInputText / saveInputText;
+    })

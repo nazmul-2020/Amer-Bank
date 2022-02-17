58b67acaa8bcd568fbcc5a1edb13bb2038ae84df
nazmul-2020 <nazmulcpi2012@gmail.com>
2022-02-17 17:30:09 +0600

    js end

--- js/app.js
+++ js/app.js
@@ -5,30 +5,32 @@ document.getElementById('cal-btn').addEventListener('click', function(){
     // ------------ Income
     const incomeInput = document.getElementById('income-input');
     const incomeInputText = parseFloat(incomeInput.value);
-    
+
     // ------------------ Food
     const foodInput = document.getElementById('food-input');
     const foodInputText = parseFloat(foodInput.value);
-    foodInput.value = '';
+        // foodInput.value = '';
+
     // ---------------- rent
     const rentInput = document.getElementById('rent-input');
     const rentInputText = parseFloat(rentInput.value);
-    rentInput.value = '';
+        // rentInput.value = '';
+
     // -------------- clothes
     const clothesInput = document.getElementById('clothes-input');
     const clothesInputText = parseFloat(clothesInput.value);
-    clothesInput.value = '';
+        // clothesInput.value = '';
 
     // -------------Total-expenses
     const totalExpensesInput = document.getElementById('total-expenses');
     const totalExpensesAmount = parseFloat(totalExpensesInput.innerText);
     const totalCost = foodInputText + rentInputText + clothesInputText;
-     totalExpensesInput.innerText = totalExpensesAmount + totalCost;
+        totalExpensesInput.innerText = totalExpensesAmount + totalCost;
     
-    // ------------------------Balance
-     const restMoneyInput = document.getElementById('rest-money');
-     const restMoneyInputAmount = parseFloat(restMoneyInput.innerText);
-     restMoneyInput.innerText = incomeInputText - totalCost;
+    // ------------------------balance
+     const balanceInput = document.getElementById('balance-money');
+     const balanceInputAmount = parseFloat(balanceInput.innerText);
+        balanceInput.innerText = incomeInputText - totalCost;
     
 
 })
@@ -36,11 +38,32 @@ document.getElementById('cal-btn').addEventListener('click', function(){
 
     document.getElementById('save-btn').addEventListener('click', function(){
 
+        
+
         const incomeInput = document.getElementById('income-input');
-        const incomeInputText = parseFloat(incomeInput.value); 
-        const saveInput = document.getElementById('save-btn');
+        const incomeInputText = parseFloat(incomeInput.value);
+        
+        const saveInput = document.getElementById('save-input');
         const saveInputText = parseFloat(saveInput.value);
+    
         const savingAmount = document.getElementById('saving-amount');
-        const savingAmountT = parseFloat(savingAmount.innerText);
-        savingAmount.innerText = incomeInputText / saveInputText;
+        // const savingAmountText = parseFloat(savingAmount.innerText);
+            savingAmount.innerText = (incomeInputText * saveInputText) / 100;
+
+           
+
+        const balanceInput = document.getElementById('balance-money');
+        const balanceInputAmount = parseFloat(balanceInput.innerText);
+        // console.log()
+        
+
+        const remainingBalanceInput = document.getElementById('remaining-Balance');
+        // const remainingBalanceAmount = parseFloat(remainingBalanceInput.innerText);
+       
+            remainingBalanceInput.innerText = balanceInputAmount - savingAmount.innerText;
+
+
+
+
+        // console.log(savingAmount)
     })
